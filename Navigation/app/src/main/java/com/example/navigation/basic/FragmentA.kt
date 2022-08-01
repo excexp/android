2c35bbe77ceb6b33c8c8d07f3a1768a41ae019fb
majianglin <majianglin@jinsanhui.com>
2022-08-01 19:13:16 +0800

    navigation basic

---
+++ Navigation/app/src/main/java/com/example/navigation/basic/FragmentA.kt
@@ -0,0 +1,27 @@
+package com.example.navigation.basic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.R
+
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.gotoFragmentB).setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentA_to_fragmentB)
+        }
+    }
+}
