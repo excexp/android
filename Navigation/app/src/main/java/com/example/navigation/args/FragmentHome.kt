c591707f12ddc1a6f64a43deb45f453b802e0d20
MaJianglin <majianglin@yihu365.com>
2022-08-01 23:20:38 +0800

    Safe Args 参数传递

--- Navigation/app/src/main/java/com/example/navigation/args/FragmentHome.kt
+++ Navigation/app/src/main/java/com/example/navigation/args/FragmentHome.kt
@@ -20,12 +20,20 @@ class FragmentHome : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.gotoDest).setOnClickListener {
+            // 不传递参数，使用默认参数
+//            findNavController().navigate(R.id.action_fragmentHome_to_fragmentDest)
+
+            // 通过 Bundle 传递参数，传统方式
 //            findNavController().navigate(R.id.action_fragmentHome_to_fragmentDest,
 //                Bundle().also {
 //                    it.putString("productId", "2")
 //                })
 
-            findNavController().navigate(R.id.action_fragmentHome_to_fragmentDest)
+
+            // 通过 Safe Args 传递参数
+            val navDestination = FragmentHomeDirections.actionFragmentHomeToFragmentDest("3")
+            findNavController().navigate(navDestination)
+
         }
     }
 }
