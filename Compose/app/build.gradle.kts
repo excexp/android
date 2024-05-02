df80f8844d9f0b5c65d2c1c5dc6fd23599974e17
excexp <excexp@gmail.com>
2024-05-02 12:35:04 +0800

    导航初步

--- Compose/app/build.gradle.kts
+++ Compose/app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.coil.kt.compose)
+    implementation(libs.androidx.navigation.compose)
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+//    implementation(libs.androidx.activity)
 
     debugImplementation(libs.androidx.ui.tooling)
 //    debugImplementation(libs.androidx.ui.test.manifest)
---
+++ Compose/app/src/main/java/com/demo/compose/navigation/GardenActivity.kt
@@ -0,0 +1,14 @@
+package com.demo.compose.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.demo.compose.R
+
+class GardenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
